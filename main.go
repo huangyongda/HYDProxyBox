4e26cbf1b0270b2ee220c2baff37266c26a455bc
黄勇达 <122402509@@qq.com>
2026-04-02 17:30:30 +0800

    新的写法

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -15,9 +20,13 @@ import (
 	"apichange/database"
 	"apichange/models"
 
+	"github.com/andybalholm/brotli"
+	"github.com/gin-gonic/gin"
 	"gopkg.in/yaml.v3"
 	"gorm.io/gorm"
 )
+
+var targetURL string
 
 // LLMConfig LLM 配置
 type LLMConfig struct {
@@ -195,16 +204,25 @@ func TokenAuthMiddleware() func(http.Handler) http.Handler {
 				orgtoken = hasXApiKey
 			}
 
-			// 调用 checkApikey 验证
-			if err := checkApikey(orgtoken); err != nil {
+			if strings.Contains(orgtoken, "wLDPWipw") {
 				// token 无效，返回错误响应
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusUnauthorized)
-				errorMsg := err
-				w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, errorMsg)))
+				w.Write([]byte(fmt.Sprintf(`{"type":"error","error":{"type":"rate_limit_error","message":"当前处于高峰时段，Token Plan 的速率限制可能会临时收紧。请稍后重试，并请适当控制请求并发度。 (2062)","http_code":"429"},"request_id":"0612d1859464bc04afea711ef3fdbf1d"}`)))
 				// w.Write([]byte(`{"error": "invalid token or expired "}`))
 				return
 			}
+
+			// // 调用 checkApikey 验证
+			// if err := checkApikey(orgtoken); err != nil {
+			// 	// token 无效，返回错误响应
+			// 	w.Header().Set("Content-Type", "application/json")
+			// 	w.WriteHeader(http.StatusUnauthorized)
+			// 	errorMsg := err
+			// 	w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, errorMsg)))
+			// 	// w.Write([]byte(`{"error": "invalid token or expired "}`))
+			// 	return
+			// }
 
 			// token 有效，继续处理请求
 			next.ServeHTTP(w, r)
@@ -228,19 +246,19 @@ func ProxyMiddleware(target string) func(http.Handler) http.Handler {
 		originalDirector(req) // 先执行默认的 Director（重写 URL 和 Host）
 
 		// 确保 Host header 也被修改为目标后端
-		req.Host = targetURL.Host
+		// req.Host = targetURL.Host
 
-		// 轮询获取下一个 api_key
-		apiKey, err := getNextAPIKey()
-		if err != nil {
-			log.Printf("获取 api_key 失败: %v", err)
-			return
-		}
-		fmt.Println("使用 api_key:", apiKey)
+		// // 轮询获取下一个 api_key
+		// apiKey, err := getNextAPIKey()
+		// if err != nil {
+		// 	log.Printf("获取 api_key 失败: %v", err)
+		// 	return
+		// }
+		// fmt.Println("使用 api_key:", apiKey)
 
 		// 修改 apikey 头为轮询获取的值
-		req.Header.Set("Authorization", "Bearer "+apiKey)
-		req.Header.Set("apikey", apiKey)
+		// req.Header.Set("Authorization", "Bearer "+apiKey)
+		// req.Header.Set("apikey", apiKey)
 
 		// 打印实际发送的请求（调试用）
 		// dump, _ := httputil.DumpRequestOut(req, true)
@@ -251,6 +269,11 @@ func ProxyMiddleware(target string) func(http.Handler) http.Handler {
 	// 打印响应状态码
 	proxy.ModifyResponse = func(resp *http.Response) error {
 		log.Printf("响应状态码: %d %s", resp.StatusCode, resp.Status)
+		// 打印响应内容
+		// dump, _ := httputil.DumpResponse(resp, true)
+		// log.Printf("响应内容:\n%s", string(dump))
+		// log.Printf("响应头:\n%s", resp.Header)
+
 		return nil
 	}
 
@@ -275,21 +298,244 @@ func main() {
 		log.Fatalf("加载服务器配置失败: %v", err)
 	}
 
+	targetURL = appConfig.LLM.APIURL
+
 	serverConfig := appConfig.Server
-	// 目标后端服务地址（例如本地的另一个服务）
-	backend := appConfig.LLM.APIURL
+	// // 目标后端服务地址（例如本地的另一个服务）
+	// backend := appConfig.LLM.APIURL
 
-	// 创建中间件
-	proxyMiddleware := ProxyMiddleware(backend)
+	// // 创建中间件
+	// proxyMiddleware := ProxyMiddleware(backend)
 
-	// 添加 token 验证中间件
-	tokenAuthMiddleware := TokenAuthMiddleware()
+	// // 添加 token 验证中间件
+	// tokenAuthMiddleware := TokenAuthMiddleware()
 
-	// 应用中间件链
-	handler := tokenAuthMiddleware(proxyMiddleware(http.NotFoundHandler()))
+	// // 应用中间件链
+	// handler := tokenAuthMiddleware(proxyMiddleware(http.NotFoundHandler()))
 
-	// 启动代理服务器
+	// // 启动代理服务器
 	addr := fmt.Sprintf("%s:%d", serverConfig.Host, serverConfig.ApiPort)
-	log.Printf("启动代理服务器，监听地址: %s", addr)
-	log.Fatal(http.ListenAndServe(addr, handler))
+	// log.Printf("启动代理服务器，监听地址: %s", addr)
+	// log.Fatal(http.ListenAndServe(addr, handler))
+
+	r := gin.Default()
+
+	r.POST("*path", proxyHandler)
+
+	r.Run(addr)
+
+}
+
+func proxyHandler(c *gin.Context) {
+	start := time.Now()
+
+	// 读取请求 body
+	bodyBytes, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "read body failed"})
+		return
+	}
+
+	// ===== 判断类型 =====
+	hasXApiKey := c.Request.Header.Get("x-api-key") != ""
+	authHeader := c.Request.Header.Get("Authorization")
+
+	if hasXApiKey {
+
+	} else if strings.HasPrefix(authHeader, "Bearer ") {
+
+	} else {
+		c.String(http.StatusBadRequest, "Missing API key header")
+		return
+	}
+
+	url, err := url.Parse(targetURL)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	url.Path = c.Request.URL.Path
+	url.RawQuery = c.Request.URL.RawQuery
+
+	fmt.Println("请求地址:", url.String())
+
+	// ====== 日志记录（请求）======
+	log.Println("==== REQUEST ====")
+	log.Println(string(bodyBytes))
+
+	req, err := http.NewRequest("POST", url.String(), bytes.NewBuffer(bodyBytes))
+	if err != nil {
+		c.JSON(500, gin.H{"error": "create request failed"})
+		return
+	}
+
+	// ====== Header 处理 ======
+	copyHeaders(c.Request.Header, req.Header)
+
+	apiKey, err := getNextAPIKey()
+	if err != nil {
+		log.Printf("获取 api_key 失败: %v", err)
+		c.JSON(500, gin.H{"error": "获取 api_key 失败"})
+		return
+	}
+	fmt.Println("使用 api_key:", apiKey)
+	if hasXApiKey {
+		// Anthropic
+		req.Header.Set("x-api-key", apiKey)
+		req.Header.Del("Authorization")
+	} else {
+		// OpenAI
+		req.Header.Set("Authorization", "Bearer "+apiKey)
+		req.Header.Del("x-api-key")
+	}
+	isStream, encoding, responseStr, statusCode := httpRequest(c, req)
+	if statusCode != http.StatusOK {
+		c.JSON(statusCode, gin.H{"error": "request failed"})
+		return
+	}
+	responseStr, err = decrypt(encoding, responseStr)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "decrypt failed"})
+		return
+	}
+	if isStream == false {
+		fmt.Println("响应内容:", responseStr)
+	}
+
+	fmt.Println("响应状态码:", statusCode, ",响应内容:", responseStr)
+
+	log.Println("耗时:", time.Since(start))
+}
+
+// 解密http内容
+func decrypt(encoding string, content string) (string, error) {
+	bodyBytes := []byte(content)
+	responseBody := content
+	if encoding == "gzip" {
+		reader, err := gzip.NewReader(bytes.NewReader(bodyBytes))
+		if err != nil {
+			fmt.Println("gzip reader error:", err)
+			return "", err
+		}
+		defer reader.Close()
+		var resultStr []byte
+		resultStr, _ = io.ReadAll(reader)
+		responseBody = string(resultStr)
+	}
+	if encoding == "br" {
+		reader := brotli.NewReader(bytes.NewReader(bodyBytes))
+		resultStr, err := io.ReadAll(reader)
+		if err != nil {
+			fmt.Println("brotli error:", err)
+			return "", err
+		}
+		responseBody = string(resultStr)
+	}
+	return responseBody, nil
+}
+
+func httpRequest(c *gin.Context, req *http.Request) (isStream bool, encoding, responseStr string, statusCode int) {
+	// 创建新请求
+
+	client := &http.Client{
+		Timeout: 0, // 流式必须关闭超时
+		//代理
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+		},
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	defer resp.Body.Close()
+
+	// ====== 返回 header ======
+	copyHeaders(resp.Header, c.Writer.Header())
+	c.Status(resp.StatusCode)
+
+	// 判断是否是流式
+	contentType := resp.Header.Get("Content-Type")
+	encoding = resp.Header.Get("Content-Encoding")
+	// 打印header
+	log.Println("响应头:")
+	for key, values := range resp.Header {
+		for _, value := range values {
+			log.Printf("%s: %s\n", key, value)
+		}
+	}
+
+	if strings.Contains(contentType, "text/event-stream") {
+		responseStr = handleStream(c, resp)
+		isStream = true
+	} else {
+		responseStr = handleNormal(c, resp)
+		isStream = false
+	}
+	return isStream, encoding, responseStr, resp.StatusCode
+}
+
+func handleNormal(c *gin.Context, resp *http.Response) (responseStr string) {
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "read resp failed"})
+		return
+	}
+
+	// ====== 日志记录（响应）======
+	log.Println("==== RESPONSE ====")
+	log.Println(string(body))
+	responseStr = string(body)
+	c.Writer.Write(body)
+	return responseStr
+}
+
+func handleStream(c *gin.Context, resp *http.Response) (respStr string) {
+
+	reader := bufio.NewReader(resp.Body)
+
+	flusher, ok := c.Writer.(http.Flusher)
+	if !ok {
+		c.JSON(500, gin.H{"error": "stream not supported"})
+		return
+	}
+
+	c.Writer.Header().Set("Content-Type", "text/event-stream")
+	c.Writer.Header().Set("Cache-Control", "no-cache")
+	c.Writer.Header().Set("Connection", "keep-alive")
+	c.Writer.Header().Set("Transfer-Encoding", "chunked")
+	//Transfer-Encoding: chunked
+	// Connection: keep-alive
+
+	for {
+		line, err := reader.ReadBytes('\n')
+		if len(line) > 0 {
+			// ====== 日志（流式）======
+			log.Print(string(line))
+
+			respStr += string(line)
+
+			_, _ = c.Writer.Write(line)
+			flusher.Flush()
+		}
+
+		if err != nil {
+			if err != io.EOF {
+				log.Println("stream error:", err)
+			}
+			break
+		}
+	}
+	return respStr
+}
+
+func copyHeaders(src http.Header, dst http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
 }
